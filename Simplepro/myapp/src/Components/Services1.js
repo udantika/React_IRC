f5a4ce2315a7faf247e6920f21a9386268c1131d
udantika <udantika912005@gmail.com>
2023-12-21 12:20:35 +0530

    Update Services1.js

--- Simplepro/myapp/src/Components/Services1.js
+++ Simplepro/myapp/src/Components/Services1.js
@@ -10,24 +10,25 @@ const Services1=()=>
 <body className='bodys'>
   <div class="cardser1">
      <h2 className='h2ser1'>
-     KPN Travels
+     KPN Cabs
      </h2>
      <p className='pser1'>
-     As a leader in South India’s bus service providers, KPN Travels shines primarily in Tamil Nadu. Having been established in 1973, KPN Travels has seen considerable growth,extending its network across Tamil Nadu, Kerala.
+     As a leader in South India’s bus service providers, KPN cabs shines primarily in Tamil Nadu. Having been established in 1973, KPN cabs has seen considerable growth,extending its network across Tamil Nadu.
 
      </p>
   </div>
   <div class="cardser1">
-    <h2 className='h2ser1'>TNSTC</h2>
+    <h2 className='h2ser1'>SN services</h2>
     <p className='pser1'>
-    Tamil Nadu State Transport Corporation (TNSTC) is a public transportation company that provides comprehensive services across Tamil Nadu and neighboring states.
+
+     SN Services is a public transportation company that provides comprehensive services across coimbatore and neighboring cities.
     Daily commuting or intercity travel, TNSTC is a trusted choice.
     </p>
   </div>
   <div class="cardser1">
-    <h2 className='h2ser1'>Neetabus IN</h2>
+    <h2 className='h2ser1'>XY Cabs</h2>
     <p className='pser1'>
-    Neeta Tours and Travels is a renowned travel company that has been providing high-quality bus services in various parts of India, including Tamil Nadu.
+   XY Services is a renowned travel company that has been providing high-quality bus services in various parts of Tamil Nadu.
     This allows passengers to conveniently book their tickets from anywhere, anytime.
     </p>
   </div>
